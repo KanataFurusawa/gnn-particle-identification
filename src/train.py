97699aff9cffa1784905fe6f2027e4443289ad78
KanataFurusawa <vj.19k.2235@s.thers.ac.jp>
2026-05-06 19:18:06 +0900

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,6 @@
+from data_loader import load_top_data
+from preprocessing import clean_photons, compute_labels
+from graph_builder import create_graph_for_event
+from model import GNNClassifier
+
+print("Training pipeline placeholder")
